21039ae26db11d70703cd791970141d2198cd60e
Usuario <Usuario@CPD03-M15>
2024-04-15 11:12:41 -0300

    Adicionar arquivos de projeto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ViagemCusto
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ViagemCusto
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
