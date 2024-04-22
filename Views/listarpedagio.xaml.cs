ece0928f918d52fa71465fb1a26546b3a1046f58
Usuario <Usuario@CPD03-M15>
2024-04-22 11:27:20 -0300

    Finalização do proejto

--- Views/listarpedagio.xaml.cs
+++ Views/listarpedagio.xaml.cs
@@ -1,9 +1,26 @@
+using System.Collections.ObjectModel;
+using ViagemCusto.Models;
+
 namespace ViagemCusto.Views;
 
 public partial class listarpedagio : ContentPage
 {
+	ObservableCollection<pedagio> lista_pedagios = new ObservableCollection<pedagio>();
 	public listarpedagio()
 	{
 		InitializeComponent();
+		lst_pedagios.ItemsSource = lista_pedagios;
+	}
+
+	protected async override void OnAppearing()
+	{
+		if(lista_pedagios.Count == 0)
+		{
+			List<pedagio> tmp = await App.Db.GetAll();
+			foreach (pedagio p in tmp)
+			{
+				lista_pedagios.Add(p);
+			}
+		}
 	}
 }
